f9e334eba5cd289e77ec3d3724ee6d60c24ea3c3
Hong-jj <79916975+Hong-jj@users.noreply.github.com>
2023-03-24 17:23:07 +0900

    Update provider.tf

--- Project_django_Terraform/provider.tf
+++ Project_django_Terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 #     shared_credentials_files = ["C:/Users/USER/.aws/credentials"]
-    shared_credentials_files = ["#/root/.aws/credentials"]
+    shared_credentials_files = ["/root/.aws/credentials"]
      
 }
 
