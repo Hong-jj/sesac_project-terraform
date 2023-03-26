2ff779935dbee1369b37e681c6b7884f6cb9f065
Hong JJ <tjdalsd66@gmail.com>
2023-03-26 21:27:12 +0900

    Backup2

--- Project_django_Terraform/provider.tf
+++ Project_django_Terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     #shared_credentials_files = ["C:/Users/USER/.aws/credentials"]
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_access_key
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_access_key}"
 }
 
 
