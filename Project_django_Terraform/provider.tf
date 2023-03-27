919d06503b4dfe1d88ac839c7305ddacf20e2fd5
Hong JJ <tjdalsd66@gmail.com>
2023-03-27 12:36:46 +0900

    Backup8

--- Project_django_Terraform/provider.tf
+++ Project_django_Terraform/provider.tf
@@ -1,7 +1,18 @@
 provider "aws" {
     #shared_credentials_files = ["C:/Users/USER/.aws/credentials"]
-    access_key = var.aws_access_key
-    secret_key = var.aws_secret_access_key
+        access_key = var.aws_access_key
+        secret_key = var.aws_secret_key
+}
+
+#Jenkins에서 변수로 받아온 Key
+variable "aws_access_key" {
+  type        = string
+  description = "AWS access key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS secret key"
 }
 
 
